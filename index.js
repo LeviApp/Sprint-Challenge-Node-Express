98853ba5e83aa5cb34e75145a01937ba0a491237
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-12-08 13:05:59 -0700

    GET implemented
    I got the GET working for the individual ids

--- index.js
+++ index.js
@@ -39,6 +39,52 @@ server.get('/api/actions', (req, res) => {
 
 })
 
+server.get('/api/projects/:id', (req, res) => {
+    const {id} = req.params;
+
+    projectDB.get(id)
+    .then(project => {
+        console.log(project)
+        if (project.length<1) {res.json(project)}
+
+        else {
+            res
+            .status(404)
+            .json({"message": "Project with that id does not exist"})
+        }
+    }
+    )
+    .catch( err => {
+        res
+        .status(500)
+        .json({"message": "Could not retrieve project."})
+    }
+    )
+
+})
+
+server.get('/api/actions/:id', (req, res) => {
+    const {id} = req.params;
+    actionDB.get(id)
+    .then(action => {
+        if (action) {res.json(action)}
+
+        else {
+            res
+            .status(404)
+            .json({"message": "Action with that id does not exist"})
+        }
+    }
+    )
+    .catch( err => {
+        res
+        .status(500)
+        .json({"message": "Could not retrieve action."})
+    }
+    )
+
+})
+
 server.listen(NUM, () => {
     console.log(`server listening on port ${NUM}`)
 })
